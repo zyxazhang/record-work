23574852b51e4ac1ec06a3bb16f2786a49a108a4
zyxazhang <17379744014@163.com>
2022-12-22 17:31:42 +0800

    minor 添加抽奖活动

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,12 @@ import 'ant-design-vue/dist/antd.css';
 import axios from './axios/index'
 import router from './router'
 import { createPinia } from 'pinia'
+import VueLuckyCanvas from '@lucky-canvas/vue'
 
 
 const app = createApp(App);
 app.config.globalProperties.$http = axios
 app.use(router)
+app.use(VueLuckyCanvas)
 app.use(createPinia())
 app.use(Antd).mount('#app');
